236f168d673cefb0cf10edff4681bd88623998c8
Angel Legorreta <legosurio@hotmail.com>
2022-03-11 07:32:47 -0600

    Gravity Config

--- Mécanica/Assets/NightmareStudio/Code/01.Player/PlayerMove.cs
+++ Mécanica/Assets/NightmareStudio/Code/01.Player/PlayerMove.cs
@@ -7,58 +7,49 @@ public class PlayerMove : MonoBehaviour
     public GameObject player;
     [SerializeField]float jumpForce;
     int jumpLimit;
-    private Vector3 theVector;
     [SerializeField] float moveVector;
     [SerializeField] float acelerationMove;
     public bool isGrounded;
     [SerializeField]float fallForce = (-9.81f);
     Vector3 moveDirection;
-
-
-    void Start()
-    {
-        jumpForce = 20;
-        moveVector = 5*(Time.deltaTime + 1);
-    }
+    Vector3 verticalDirection;
 
     void Update()
     {
-        RightMovement();
-        LeftMovement();
-        //Debug.Log(gameObject.transform.position.magnitude);
         CheckFloar();
-        fall();
-        gameObject.transform.position += theVector * Time.deltaTime;
-        gameObject.transform.position += moveDirection * Time.deltaTime;
+        Debug.Log(gameObject.transform.position.magnitude);
+        Movement();
         Jump();
-        Down();
-        if(!isGrounded)
-        {
-            jumpForce = -(jumpForce - fallForce);
-            fallForce += ((-9.81f * Time.deltaTime));
-        }
-        else if(isGrounded)
-        {
-            fallForce = 0;
-            jumpForce = 20;
-        }
+        Fall();
+        
     }
     
-     void RightMovement()
+     void Movement()
      {
+        moveVector = 5*(Time.deltaTime + 1);
+
          if (Input.GetKeyDown(KeyCode.D))
          {
              Vector3 rightMove = new Vector3 (moveVector,0,0);
              moveDirection += (rightMove);
          }
-     }
-     void LeftMovement()
-     {
          if (Input.GetKeyDown(KeyCode.A))
          {
              Vector3 rightMove = new Vector3 (moveVector,0,0);
              moveDirection -= (rightMove);
          }
+         gameObject.transform.position += (moveDirection * Time.deltaTime)+((moveDirection/10) * Time.deltaTime);
+         if (Input.GetKeyUp(KeyCode.D))
+         {
+             Vector3 rightMove = new Vector3 (moveVector,0,0);
+             moveDirection += (-rightMove/3);
+         }
+         if (Input.GetKeyUp(KeyCode.A))
+         {
+             Vector3 rightMove = new Vector3 (moveVector,0,0);
+             moveDirection -= (rightMove/3);
+         }
+         gameObject.transform.position += (moveDirection * Time.deltaTime)+((moveDirection/10) * Time.deltaTime);
      }
 
     void Jump()
@@ -66,28 +57,18 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             Vector3 jump = new Vector3 (0, jumpForce, 0);
-            if(jumpForce >= 0.1)
-                {
-                    theVector += (jump);
-                }
-                else if(jumpForce <= 0)
-                    {
-                        theVector -= jump;
-                    }
-            Debug.Log("jumping >D");
-        }   
-    }
-    void Down()
-    {
+            verticalDirection += jump*Time.deltaTime;
+        }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            Vector3 jump = new Vector3 (0, theVector.y, 0);
-            theVector -= jump;
-            Debug.Log("AntiSaltanding");
+            Vector3 jump = new Vector3 (0, jumpForce, 0);
+            Vector3 falling = new Vector3 (0, fallForce, 0);
+            verticalDirection += ((-jump)+(falling/5))*Time.deltaTime;
         }
+        gameObject.transform.position += (verticalDirection * (Time.deltaTime/3));
     }
 
-    void fall()
+    void Fall()
     {
         Vector3 falling = new Vector3(0,fallForce, 0); 
         if(!isGrounded)
@@ -96,22 +77,26 @@ public class PlayerMove : MonoBehaviour
         }
         else
         {
-             gameObject.transform.position += (falling * -1) * Time.deltaTime;
+             gameObject.transform.position += (falling * -1);
         }
     }
 
     void CheckFloar()
     {
+        
+        
+        
         float previo;
         if(Physics.Raycast(gameObject.transform.position,Vector3.down,0.5f))
         {
             isGrounded = true;
             previo = fallForce;
             fallForce = 0;
+            jumpForce = 20*(Time.deltaTime + 1);
         }
         else
         {
-            fallForce = -9.81f;
+            fallForce = 10*(Time.deltaTime);
             isGrounded = false;  
         }
     }
