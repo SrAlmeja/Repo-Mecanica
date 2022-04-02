4807749784267795fd3c039ff5ad6d17b5d98981
Angel Legorreta <legosurio@hotmail.com>
2022-04-01 19:30:02 -0600

    New Scripts
    Se nueva version de gravedad y movimiento

---
+++ Mécanica/Assets/NightmareStudio/Code/01.Player/GravityFisics.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFisics : MonoBehaviour
+{
+    float fallSpeed;
+    bool isGrounded;
+    Vector3 gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFloar();
+        GravitySystem();
+    }
+
+    void GravitySystem()
+    {
+        fallSpeed -= 0.0981f * Time.deltaTime;
+        Vector3 falling = new Vector3(0,fallSpeed, 0);
+        if (!isGrounded)
+        {   
+            gameObject.transform.position += (falling * -fallSpeed);
+        }
+    }
+
+    void CheckFloar()
+    {
+        if(Physics.Raycast(gameObject.transform.position,Vector3.down,0.5f))
+        {
+            isGrounded = true;
+            fallSpeed=0;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+}
