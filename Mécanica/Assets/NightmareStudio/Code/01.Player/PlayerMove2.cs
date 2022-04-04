b4afefd6627c31518a9218d9bf637afe37042ae7
Angel Legorreta <legosurio@hotmail.com>
2022-04-04 01:56:43 -0500

    movement2

--- Mécanica/Assets/NightmareStudio/Code/01.Player/PlayerMove2.cs
+++ Mécanica/Assets/NightmareStudio/Code/01.Player/PlayerMove2.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMove2 : MonoBehaviour
 {
+    
+    public bool isGrounded;
+    [SerializeField]float jumpForce;
+    Vector3 verticalDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,36 @@ public class PlayerMove2 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Movement()
+    {
+
+    }
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            Vector3 jump = new Vector3 (0, jumpForce, 0);
+        }
+        gameObject.transform.position += (verticalDirection) * Time.deltaTime;
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 jump = new Vector3 (0, jumpForce, 0);
+            
+            verticalDirection -= (jump);
+        }
+    }
+
+    void CheckFloar()
+    {
+        if(Physics.Raycast(gameObject.transform.position,Vector3.down,0.5f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;  
+        }
     }
 }
