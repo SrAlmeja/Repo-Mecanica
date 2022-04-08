b7affd166ed83f8b1567b6a183fa0e6fe35bf380
Angel Legorreta <legosurio@hotmail.com>
2022-04-07 20:06:07 -0500

    player rotation

--- Mécanica/Assets/NightmareStudio/Code/01.Player/GravityPlanetFisics.cs
+++ Mécanica/Assets/NightmareStudio/Code/01.Player/GravityPlanetFisics.cs
@@ -10,6 +10,7 @@ public class GravityPlanetFisics : MonoBehaviour
    float fallSpeed;
    Vector3 planetPosition;
    Vector3 PlayerPosition;
+   Vector3 gravity;
    //CheckFloar
    public bool isGrounded;
    //Rotation figure
@@ -27,25 +28,21 @@ public class GravityPlanetFisics : MonoBehaviour
     {
         CheckFloar();
         GravityFisics();
-        RotateFigure();
     }
 
 
     void GravityFisics()
     {
         fallSpeed -= 0.0981f * Time.deltaTime;
-        Vector3 planetPos = new Vector3 (planets[0].transform.position.x,planets[0].transform.position.y,planets[0].transform.position.z);
-        Vector3 playerPos = new Vector3 (player.transform.position.x,player.transform.position.y,player.transform.position.z);
-        Vector3 gravityDirection = new Vector3 (((playerPos.x - planetPos.x)*fallSpeed),((playerPos.y - planetPos.y)*fallSpeed),((playerPos.z - planetPos.z)*fallSpeed));
+        Vector3 planetPos = new Vector3 (planets[0].transform.position.x,planets[0].transform.position.y,0);
+        Vector3 playerPos = new Vector3 (player.transform.position.x,player.transform.position.y,0);
+        Vector3 gravityDirection = new Vector3 ((playerPos.x - planetPos.x),(playerPos.y - planetPos.y),0);
         Vector3 gravity = new Vector3 (gravityDirection.x, gravityDirection.y, 0);
         if(!isGrounded)
         {
-            gameObject.transform.position += (gravity * -fallSpeed);
+            gameObject.transform.position += (gravity * fallSpeed);
         }
-    }
-    void RotateFigure()
-    {
-        
+        player.transform.rotation = Quaternion.FromToRotation(transform.up, gravity) * transform.rotation;
     }
 
     void CheckFloar()
