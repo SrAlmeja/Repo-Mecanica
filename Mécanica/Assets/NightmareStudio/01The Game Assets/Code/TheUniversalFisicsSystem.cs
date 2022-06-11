b31bb93f272b649b3776be6238a97bf28f8fa13e
Angel Legorreta <legosurio@hotmail.com>
2022-06-10 22:22:44 -0500

    Diseño de nivel completo

---
+++ Mécanica/Assets/NightmareStudio/01The Game Assets/Code/TheUniversalFisicsSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheUniversalFisicsSystem : MonoBehaviour
+{
+    //Planets Detection
+    Vector3 planetPos;
+    Vector3 objectPos;
+    public gameObject theObject;
+    
+    //Vertical Gravity
+    //Universal Gravity 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PlanetDistance(){
+
+    }
+    void DragSystem(){
+
+    }
+    void VerticalGravity(){
+
+    }
+    void GravitySystem(){
+
+    }
+
+    void ColisionSystem(){
+
+    }
+}
